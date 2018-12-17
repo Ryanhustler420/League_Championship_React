8d028b0a21dba012637177cbfc048966a6609d9d
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-17 21:46:59 +0530

    Adding a Header

---
+++ app/src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = () => {
+  return (
+    <Layout>
+      Second Line
+    </Layout>
+  )
+}
+
+export default Routes;
