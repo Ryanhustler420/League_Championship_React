69045ec91ac8c9d1d4c865582008cd5a62c946a6
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-18 21:26:09 +0530

    Home Meet the players component

--- app/src/Components/home/index.js
+++ app/src/Components/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Featured from './featured/index';
 import MatchesHome from './matches';
+import MeetPlayers from './meetPlayers';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured />
             <MatchesHome />
+            <MeetPlayers />
         </div>
     )
 }
