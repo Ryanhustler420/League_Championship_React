456d7403e00c02f3a4fa1f51d4595cb5d9bc9bdf
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-19 00:41:33 +0530

    Home Finishing player card...continued

--- app/src/Components/home/meetPlayers/cards.js
+++ app/src/Components/home/meetPlayers/cards.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
+import PlayerCard from './../../UI/PlayerCard';
 
 export default class HomeCards extends Component {
 
@@ -49,7 +50,12 @@ export default class HomeCards extends Component {
                                 bottom
                             }}
                         >
-                            div
+                            <PlayerCard 
+                                number="30"
+                                name="Nicolas"
+                                lastname="Otamendi"
+                                bck={Otamendi}
+                            />
                         </div>
                     )
                 }}
