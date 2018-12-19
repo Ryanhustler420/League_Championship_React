5e2fca61d9c9978cef03e77d3bedc583c93c4224
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-20 00:35:43 +0530

    Admin Edit Match - creating the form 2

--- app/src/Components/admin/matches/addEditMatch.js
+++ app/src/Components/admin/matches/addEditMatch.js
@@ -44,6 +44,122 @@ export default class AddEditMatch extends Component {
                 valid: false,
                 validationMessage: '',
                 showLabel: false
+            },
+            resultLocal:{
+                element: 'input',
+                value:'',
+                config: {
+                    label: 'Result Local',
+                    name: 'result_local_input',
+                    type: 'text'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            away:{
+                element: 'select',
+                value:'',
+                config: {
+                    label: 'Select a Away team',
+                    name: 'select_Away',
+                    type: 'select',
+                    options: []
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            resultAway:{
+                element: 'input',
+                value:'',
+                config: {
+                    label: 'Result Local',
+                    name: 'result_local_input',
+                    type: 'text'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            referee:{
+                element: 'input',
+                value:'',
+                config: {
+                    label: 'Referee',
+                    name: 'referee_input',
+                    type: 'text'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: true
+            },
+            stadium:{
+                element: 'input',
+                value:'',
+                config: {
+                    label: 'Stadium',
+                    name: 'Stadium_input',
+                    type: 'text'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: true
+            },
+            result:{
+                element: 'select',
+                value:'',
+                config: {
+                    label: 'Team result',
+                    name: 'select_result',
+                    type: 'select',
+                    options: [
+                        { key: 'W', value: 'W' },
+                        { key: 'L', value: 'L' },
+                        { key: 'D', value: 'D' },
+                        { key: 'n/a', value: 'n/a' }
+                    ]
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: true
+            },
+            final:{
+                element: 'select',
+                value:'',
+                config: {
+                    label: 'Game played ?',
+                    name: 'select_played',
+                    type: 'select',
+                    options: [
+                        { key: 'Yes', value: 'Yes' },
+                        { key: 'No', value: 'No' },
+                    ]
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: true
             }
         }
     }
@@ -64,6 +180,95 @@ export default class AddEditMatch extends Component {
                                 formData={this.state.formData.date}
                                 change={(e) => this.updateForm(e)}
                             />
+
+                            <div className="select_team_layout">
+                                <div className="label_inputs">Local</div>
+                                <div className="wrapper">
+                                    <div className="left">
+                                        <FormInput 
+                                            id={'local'}
+                                            formData={this.state.formData.local}
+                                            change={(e) => this.updateForm(e)}
+                                        />
+                                    </div>
+                                    <div>
+                                        <FormInput 
+                                            id={'resultLocal'}
+                                            formData={this.state.formData.resultLocal}
+                                            change={(e) => this.updateForm(e)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="select_team_layout">
+                                <div className="label_inputs">Away</div>
+                                <div className="wrapper">
+                                    <div className="left">
+                                        <FormInput 
+                                            id={'away'}
+                                            formData={this.state.formData.away}
+                                            change={(e) => this.updateForm(e)}
+                                        />
+                                    </div>
+                                    <div>
+                                        <FormInput 
+                                            id={'resultAway'}
+                                            formData={this.state.formData.resultAway}
+                                            change={(e) => this.updateForm(e)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="split_fields">
+                                <FormInput 
+                                    id={'referee'}
+                                    formData={this.state.formData.referee}
+                                    change={(e) => this.updateForm(e)}
+                                />
+
+                                <FormInput 
+                                    id={'stadium'}
+                                    formData={this.state.formData.stadium}
+                                    change={(e) => this.updateForm(e)}
+                                />
+                            </div>
+
+                            <div className="split_fields last">
+                                <FormInput 
+                                    id={'result'}
+                                    formData={this.state.formData.result}
+                                    change={(e) => this.updateForm(e)}
+                                />
+
+                                <FormInput 
+                                    id={'final'}
+                                    formData={this.state.formData.final}
+                                    change={(e) => this.updateForm(e)}
+                                />
+
+                            </div>
+
+                            <div className="success_label">
+                                {this.state.formSuccess}
+                            </div>
+
+                            {
+                                this.state.formError ? 
+                                    <div className="error_label">
+                                        Something is wrong
+                                    </div>
+                                : ''
+                            }
+
+                            <div className="admin_submit">
+                                <button onClick={(event) => this.submitForm(event)}>
+                                    {this.state.formType}
+                                </button>
+                            </div>
+
                         </form>
                     </div>
                 </div>
