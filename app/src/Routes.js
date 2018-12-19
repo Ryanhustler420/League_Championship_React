b2feb57940aaa7b8eb0b43e01b40c7f58a825ff2
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-20 02:14:13 +0530

    Admin Players Endpoint

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -8,6 +8,7 @@ import PrivateRoutes from './Components/authRoutes/privateRoutes';
 import PublicRoutes from './Components/authRoutes/publicRoutes';
 import AdminMatches from './Components/admin/matches/index';
 import AddEditMatch from './Components/admin/matches/addEditMatch';
+import AdminPlayers from './Components/admin/players/index';
 
 const Routes = (props) => {
 
@@ -19,6 +20,7 @@ const Routes = (props) => {
         <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
         <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PrivateRoutes {...props} path="/dashboard" exact component={DashBoard}/>
+        <PrivateRoutes {...props} path="/admin_players" exact component={AdminPlayers}/>
         <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches}/>
         <PrivateRoutes {...props} path="/admin_matches/edit_match" exact component={AddEditMatch}/>
         <PrivateRoutes {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
