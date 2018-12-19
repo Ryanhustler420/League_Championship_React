a10b813c35030d23741538d551b60c092a0f23a1
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-19 16:58:09 +0530

    Auth Private Routes

---
+++ app/src/Components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLayout from './../../Hoc/AdminLayout';
+
+const DashBoard = () => {
+    return (
+        <AdminLayout>
+            <div className="user_dashboard">
+                <div>This is your dashboard</div>
+            </div>
+        </AdminLayout>
+    )
+}
+
+export default DashBoard
--- app/src/Routes.js
+++ app/src/Routes.js
@@ -3,6 +3,7 @@ import Layout from './Hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/home';
 import SignIn from './Components/signin/index';
+import DashBoard from './Components/admin/Dashboard';
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/sign_in" exact component={SignIn}/>
+        <Route path="/dashboard" exact component={DashBoard}/>
       </Switch>
     </Layout>
   )
