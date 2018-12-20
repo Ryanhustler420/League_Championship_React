2c33a0eec988c7c45fbf87a53881459144f5a426
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-20 19:20:38 +0530

    Admin Players fileuploader ..continued

--- app/src/Components/UI/FileUploader.js
+++ app/src/Components/UI/FileUploader.js
@@ -13,6 +13,34 @@ export default class Fileuploader extends Component {
         fileURL: ''
     }
 
+    handleUploadStart = () => {
+        this.setState({
+            isUploading: true
+        });
+    }
+
+    handleUploadError = () => {
+        this.setState({
+            isUploading: false
+        })
+    }
+
+    handleUploadSuccess = (filename) => {
+        
+        this.setState({
+            name: filename,
+            isUploading: false
+        });
+
+        firebase.storage().ref(this.props.dir)
+        .child(filename).getDownloadURL()
+        .then(url => {
+            this.setState({
+                fileURL: url
+            })
+        })
+    }
+
     static getDerivedStateFromProps(props, state) {
         if(props.defaultImg){
             return state = {
@@ -44,6 +72,34 @@ export default class Fileuploader extends Component {
 
                     :null
 
+                }
+                {
+                    this.state.isUploading ? 
+                        <div className="progress"
+                            style={{textAlign:'center', margin: '30px 0'}}
+                        >
+                            <CircularProgress 
+                                style={{color: '#98c6e9'}}
+                                thickness={7}
+                            />
+                        </div>
+                    : null
+                }
+                {
+                    this.state.fileURL ?
+                        <div className="image_upload_container">
+                            <img 
+                                style={{
+                                    width: '100%'
+                                }}
+                                src={this.state.fileURL}
+                                alt={this.state.name}
+                            />
+                            <div className="remove" onClick={() => this.uploadAgain()}>
+                                Remove
+                            </div>
+                        </div>
+                    :null
                 }
             </div>
         )
